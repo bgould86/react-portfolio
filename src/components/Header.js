64d9e8afc29b7e084806b04a032ea0d156c3fef1
bgould86 <bgould86@gmail.com>
2022-05-07 13:31:08 -0700

    really setting up now

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <>
+      <header>
+        logo here
+        <nav>Projects Contact Resume</nav>
+      </header>
+    </>
+  );
+}
